d8053e921cf0f8359e51e6277068f6cda6b0d017
Ira Abramov <ira+gpg2017@lightbitslabs.com>
2017-10-22 16:28:49 +0300

    Rethinking this in a new way.

--- themes/ia42/ia42.theme.bash
+++ themes/ia42/ia42.theme.bash
@@ -1,27 +1,63 @@
+#!/usr/bin/env bash
 # port of rjurgensen which is # port of zork theme
+# Plus bits borrowed from powerline-multiline
 
+. "$BASH_IT/themes/powerline-multiline/powerline-multiline.base.bash"
+
+### Color and gliph settings
 # set colors for use throughout the prompt
-# i like things consistent
+# I like things consistent
 BRACKET_COLOR=${blue}
 STRING_COLOR=${green}
+PROMPT_CHAR=""
+[[ $(id -u) -eq 0 ]] && PROMPT_CHAR="${bold_red}#"
 
-SCM_THEME_PROMPT_PREFIX=""
-SCM_THEME_PROMPT_SUFFIX=""
+USER_INFO_SSH_CHAR=${POWERLINE_USER_INFO_SSH_CHAR:=" "}
+USER_INFO_THEME_PROMPT_COLOR=32
+USER_INFO_THEME_PROMPT_COLOR_SUDO=202
+unset GIT_THEME_PROMPT_DIRTY
+unset SCM_THEME_PROMPT_SUFFIX
+unset SCM_THEME_PROMPT_PREFIX
+SCM_GIT_SHOW_MINIMAL_INFO=false
+SCM_GIT_SHOW_DETAILS=true
 
-SCM_THEME_PROMPT_DIRTY=" ${bold_red}✗${normal}"
+PYTHON_VENV_CHAR=${POWERLINE_PYTHON_VENV_CHAR:="❲p❳ "}
+CONDA_PYTHON_VENV_CHAR=${POWERLINE_CONDA_PYTHON_VENV_CHAR:="❲c❳ "}
+PYTHON_VENV_THEME_PROMPT_COLOR=35
+
+#SCM_NONE_CHAR=""
+SCM_GIT_CHAR="${bold_yellow}${normal}"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
-SCM_GIT_CHAR="${STRING_COLOR}±${normal}"
+SCM_THEME_PROMPT_DIRTY=" ${bold_red}✗${normal}"
+SCM_THEME_PROMPT_CLEAN_COLOR=25
+SCM_THEME_PROMPT_DIRTY_COLOR=88
+SCM_THEME_PROMPT_STAGED_COLOR=30
+SCM_THEME_PROMPT_UNSTAGED_COLOR=92
+SCM_THEME_PROMPT_COLOR=${SCM_THEME_PROMPT_CLEAN_COLOR}
 SCM_SVN_CHAR="${bold_cyan}⑆${normal}"
 SCM_HG_CHAR="${bold_red}☿${normal}"
 
-case $TERM in
-    xterm*|rxvt*)
-        TITLEBAR='\[\033]0;\u@\h:\w\007\]'
-        ;;
-    *)
-        TITLEBAR=""
-        ;;
-esac
+RVM_THEME_PROMPT_PREFIX=""
+RVM_THEME_PROMPT_SUFFIX=""
+RBENV_THEME_PROMPT_PREFIX=""
+RBENV_THEME_PROMPT_SUFFIX=""
+RUBY_THEME_PROMPT_COLOR=161
+RUBY_CHAR=${POWERLINE_RUBY_CHAR:="❲r❳ "}
+
+CWD_THEME_PROMPT_COLOR=240
+
+LAST_STATUS_THEME_PROMPT_COLOR=196
+
+IN_VIM_THEME_PROMPT_COLOR=245
+IN_VIM_THEME_PROMPT_TEXT="vim"
+
+#POWERLINE_LEFT_PROMPT=${POWERLINE_LEFT_PROMPT:="scm python_venv ruby cwd"}
+#POWERLINE_RIGHT_PROMPT=${POWERLINE_RIGHT_PROMPT:="in_vim clock battery user_info"}
+POWERLINE_LEFT_PROMPT="scm python_venv ruby cwd in_vim user_info"
+POWERLINE_LEFT_PROMPT="scm cwd in_vim user_info"
+unset POWERLINE_RIGHT_PROMPT
+
+#safe_append_prompt_command __powerline_prompt_command
 
 function clientp() {
     [[ "$clientprompt" ]] && \
@@ -70,21 +106,12 @@ todo_txt_count() {
 }
 
 modern_scm_prompt() {
-        CHAR=$(scm_char)
-        if [ $CHAR = $SCM_NONE_CHAR ]
-        then
-                return
-        else
-                echo "${BRACKET_COLOR}[${CHAR} ${STRING_COLOR}$(scm_prompt_info)${BRACKET_COLOR}]$normal"
-        fi
-}
-
-my_prompt_char() {
-    if [[ $OSTYPE =~ "darwin" ]]; then
-        echo "${BRACKET_COLOR}➞  ${normal}"
-    else
-        echo "${BRACKET_COLOR}➞ ${normal}"
-    fi
+  CHAR=$(scm_char)
+  if [ 'x$CHAR' = 'x$SCM_NONE_CHAR' ]; then
+    return
+  else
+    echo "${BRACKET_COLOR}[${CHAR} ${STRING_COLOR}$(scm_prompt_info)${BRACKET_COLOR}]$normal"
+  fi
 }
 
 prompt() {
@@ -96,18 +123,12 @@ prompt() {
     my_ps_root="${bold_red}\u${normal}";
     my_ps_path="${STRING_COLOR}\w${normal}";
 
-    # nice prompt
-    case "`id -u`" in
-        0) PS1="${TITLEBAR}${BRACKET_COLOR}┌─$(clientp)[$my_ps_root${BRACKET_COLOR}@$my_ps_host${BRACKET_COLOR}]$(modern_scm_prompt)$(__my_rvm_ruby_version)${BRACKET_COLOR}[${STRING_COLOR}\w${BRACKET_COLOR}]$(is_vim_shell)
-${BRACKET_COLOR}└─$(lastret $ret)$(todo_txt_count)$(my_prompt_char)${normal}"
-        ;;
-        *) PS1="${TITLEBAR}${BRACKET_COLOR}┌─$(clientp)[$my_ps_user${BRACKET_COLOR}@$my_ps_host${BRACKET_COLOR}]$(modern_scm_prompt)$(__my_rvm_ruby_version)${BRACKET_COLOR}[${STRING_COLOR}\w${BRACKET_COLOR}]$(is_vim_shell)
-${BRACKET_COLOR}└─$(lastret $ret)$(todo_txt_count)$(my_prompt_char)${normal}"
-        ;;
-    esac
+    PS1="${TITLEBAR}${BRACKET_COLOR}$(clientp)[$my_ps_root${BRACKET_COLOR}@$my_ps_host${BRACKET_COLOR}]$(modern_scm_prompt)$(__my_rvm_ruby_version)${BRACKET_COLOR}[${STRING_COLOR}\w${BRACKET_COLOR}]$(is_vim_shell)
+$(lastret $ret)${PROMPT_CHAR}${normal} "
+
 }
 
-PS2="└─$(my_prompt_char)"
+PS2="${PROMPT_CHAR}${normal} "
 
 
 
